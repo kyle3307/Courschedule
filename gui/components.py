808ade08287e568e3e26481ab5ecdbb7d6132799
kyle3307 <kyle3307@outlook.com>
2024-12-01 13:39:29 +0800

    修复schedule.json导致的Sunday显示错误的bug

--- gui/components.py
+++ gui/components.py
@@ -3,18 +3,20 @@ from logic.configure import config
 
 
 def create_schedule_entries(periods, parent):
-    conf=config()
+    conf = config()
     fonts = conf.get("fonts")
     entries = []
     for _ in range(periods):
-        entry = tk.Entry(parent, font=tuple(fonts["lessons_font"]), justify="center")
+        entry = tk.Entry(parent,
+                         font=tuple(fonts["lessons_font"]),
+                         justify="center")
         entry.pack()
         entries.append(entry)
     return entries
 
 
 def create_buttons(parent, save_callback, draw_callback):
-    conf=config()
+    conf = config()
     fonts = conf.get("fonts")
     save_button = tk.Button(parent,
                             text="保存",
